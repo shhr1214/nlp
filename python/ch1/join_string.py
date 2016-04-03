383760a1e6d79b8c10b26a6466fc74c28f960792
Sohei Hara <souhei0079@gmail.com>
2016-04-04 08:38:21 +0900

    add

---
+++ python/ch1/join_string.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    pato = 'パトカー'
+    taku = 'タクシー'
+    ans = ''
+    for i in range(0,4):
+        ans += pato[i]
+        ans += taku[i]
+    print(ans)
---
+++ python/ch1/pi.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    pi = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+    pi_list = pi.split(" ")
+    ans_list = [x[0] for x in pi_list]
+    print(ans_list)
