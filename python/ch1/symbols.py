0f372aea61bc2dd5496c2573229882daecb1159e
Sohei Hara <souhei0079@gmail.com>
2016-04-07 08:34:54 +0900

    modify symbols.py

--- python/ch1/symbols.py
+++ python/ch1/symbols.py
@@ -1,9 +1,12 @@
 symbols = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.".split(" ")
 
 nums = {1, 5, 6, 7, 8, 9, 15, 16, 19}
+dict = {}
 
 for i in range(len(symbols)):
     if i+1 in nums:
-        print(symbols[i][0])
+        dict[symbols[i][0]] = i + 1
     else:
-        print(symbols[i][0:2])
+        dict[symbols[i][0:2]] = i + 1
+
+print(dict)
