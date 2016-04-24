7a9c1771af7796df798cb71f1fd92dbe3e0ea47e
Sohei Hara <souhei0079@gmail.com>
2016-04-25 07:43:31 +0900

    add 02

--- clj/ch1.clj
+++ clj/ch1.clj
@@ -1,10 +1,19 @@
 (ns ch1
   (:require [clojure.string :as s]))
 
-;; 01
+(def patrol-car "パトカー")
+(def taxi "タクシー")
+(def pi "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.")
+
+;; 00
 (defn reverse-string [str]
   (s/reverse str))
 
-;; 02
+;; 01
 (defn extract-string [s1]
-  (apply str (take-nth 2 s1)))
+  (s/join (take-nth 2 s1)))
+
+;; 02
+(defn join-string [s1 s2]
+  (s/join (interleave s1 s2)))
+
