aec5f817308d096d3a53d5c9cc8acabd2537542d
Sohei Hara <souhei0079@gmail.com>
2016-05-06 16:45:00 +0900

    add 12

--- clj/ch2.clj
+++ clj/ch2.clj
@@ -8,11 +8,36 @@
   (with-open [rdr (clojure.java.io/reader path)]
     (count (line-seq rdr))))
 
-(println (count-line f))
+;; (println (count-line f))
 
 ;; 11
 (defn tab2space [path]
   (let [content (slurp path)]
     (cstr/replace content #"\t" " ")))
 
-(println (tab2space f))
+;; (println (tab2space f))
+
+;; 12
+(defn txt->seq [path]
+  (with-open [rdr (clojure.java.io/reader path)]
+    (let [lines (line-seq rdr)]
+      (doall (seq lines)))))
+
+(defn get-nth-from-line [lines n]
+  (loop [lines lines
+         result []]
+    (if (empty? lines)
+      result
+      (recur (rest lines)
+             (conj result (nth (cstr/split (first lines) #"\t") n))))))
+
+(defn split-txt [path]
+  (let [col1 (get-nth-from-line (txt->seq path) 0)
+        col2 (get-nth-from-line (txt->seq path) 1)]
+    (do
+      (spit "col1.txt" (cstr/join "\n" col1))
+      ;; (println (cstr/join #"\n" col1))
+      (spit "col2.txt" (cstr/join "\n" col2)))))
+      ;; (println (cstr/join #"\n" col2)))))
+
+;; (split-txt f)
